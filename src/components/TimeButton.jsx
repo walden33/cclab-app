fb6029f6a75d12909ac0954e4667dddbb21ad445
liyuan31 <admin@waldenli.com>
2022-06-23 18:16:15 -0400

    Apply color to time button

--- src/components/TimeButton.jsx
+++ src/components/TimeButton.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 
 export default function TimeButton(props) {
-    const [free, setFree] = useState();
+    const [free, setFree] = useState(false);
+    const handleClick = (e) => {
+        props.updateFunction(e);
+        setFree(!free);
+    };
     return (
         <td
-            className="text-center border border-slate-400"
+            className={"text-center border border-slate-400 ".concat(
+                free ? "bg-green-300" : "bg-red-300"
+            )}
             id={props.id}
-            onClick={props.updateFunction}
+            onClick={handleClick}
         >
             {free ? "Yes" : "No"}
         </td>
