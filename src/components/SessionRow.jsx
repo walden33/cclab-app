788dc40bd9e69b842ab7194b43a466419cf93303
liyuan31 <admin@waldenli.com>
2022-06-24 09:01:12 -0400

    Create component

---
+++ src/components/SessionRow.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SessionRow = (props) => {
+    return (
+        <tr className="border-b">
+            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                {props.code}
+            </td>
+            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {props.start}
+            </td>
+            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {props.end}
+            </td>
+            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {props.ra}
+            </td>
+        </tr>
+    );
+};
+
+export default SessionRow;
