77d38f087ad16482c007c7a0ef90c8663e070fcd
liyuan31 <admin@waldenli.com>
2022-08-04 14:34:42 -0400

    Convert dateTime to firestore date objects

--- src/components/AddSession.jsx
+++ src/components/AddSession.jsx
@@ -1,12 +1,30 @@
 import React, { useState } from "react";
+import { db } from "../firebase";
+import { collection, addDoc, Timestamp } from "firebase/firestore";
 
 const AddSession = () => {
     const [email, setEmail] = useState("");
     const [startTime, setStartTime] = useState();
     const [endTime, setEndTime] = useState();
     const [sessionCode, setSessionCode] = useState("");
+    const [researcher, setResearcher] = useState("");
     const [compensation, setCompensation] = useState(0);
-    const handleSubmit = () => {};
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const docRef = await addDoc(collection(db, "sessions"), {
+                subId: email,
+                sessionCode: sessionCode,
+                researcher: researcher,
+                startTime: Timestamp.fromDate(new Date(startTime)),
+                endTime: Timestamp.fromDate(new Date(endTime)),
+                compensation: compensation,
+                status: "open",
+            });
+        } catch (error) {
+            alert(error);
+        }
+    };
     return (
         <div className="max-w-[700px] mx-auto my-16 p-4">
             <form onSubmit={handleSubmit}>
@@ -22,6 +40,14 @@ const AddSession = () => {
                     <label className="py-2 font-medium">Session Code</label>
                     <input
                         onChange={(e) => setSessionCode(e.target.value)}
+                        className="border p-3"
+                        type="text"
+                    />
+                </div>
+                <div className="flex flex-col py-2">
+                    <label className="py-2 font-medium">Researcher Name</label>
+                    <input
+                        onChange={(e) => setResearcher(e.target.value)}
                         className="border p-3"
                         type="text"
                     />
@@ -51,7 +77,10 @@ const AddSession = () => {
                     />
                 </div>
                 <div className="flex flex-col py-2">
-                    <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 text-white w-60 p-4 my-2">
+                    <button
+                        onClick={handleSubmit}
+                        className="border border-blue-500 bg-blue-600 hover:bg-blue-500 text-white w-60 p-4 my-2"
+                    >
                         Book Session
                     </button>
                 </div>
