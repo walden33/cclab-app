a04e5fa88cb3484bc31dca1c8714aac13a67f351
liyuan31 <admin@waldenli.com>
2022-08-07 19:25:46 -0400

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-center text-3xl font-bold my-10">CCLab</h1>
+        </div>
+    );
+};
+
+export default Header;
