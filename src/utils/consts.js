c7e515027563928d7060d9f2a64dc397ba3565e5
liyuan31 <admin@waldenli.com>
2022-08-13 14:49:32 -0400

    Add more consts

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,2 +1,15 @@
+// Google Calendar IDs
+export const CALENDAR_IDS = {
+    FMRI: process.env.REACT_APP_CALENDAR_ID_FMRI,
+    RM1: process.env.REACT_APP_CALENDAR_ID_RM1,
+    RM2: process.env.REACT_APP_CALENDAR_ID_RM2,
+    RM3: process.env.REACT_APP_CALENDAR_ID_RM3,
+    RM221E: process.env.REACT_APP_CALENDAR_ID_RM221E,
+};
+
+// URL of the Google Calendar API endpoint
+export const CALENDAR_EVENT_API_URL =
+    process.env.REACT_APP_CALENDAR_EVENT_API_URL;
+
 // Location of Lazenby Hall, used in creating Google Calendar event
 export const LOCATION = "1827 Neil Ave, Columbus, OH 43210";
