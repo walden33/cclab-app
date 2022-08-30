b6492c743766d3fb2cebb449ee9f3337878afa8c
Walden31 <admin@waldenli.com>
2022-08-30 15:29:11 -0400

    Install react-spinners

---
+++ src/components/Onboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Onboard = () => {
+    return (
+        <div className="flex flex-col border border-black p-4">
+            <h1 className="text-2xl font-bold py-4">Next Steps</h1>
+            <p>1. Tell us your availability</p>
+            <p>
+                2. One of our team members will reach out to you for additional
+                information
+            </p>
+        </div>
+    );
+};
+
+export default Onboard;
